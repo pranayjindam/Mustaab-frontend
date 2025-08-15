271e9edb82d60c33222da447b5bf59c8c1ceb726
Pranay Jindam <pranayjindam0708@gmail.com>
2025-08-15 12:32:11 +0530

    local

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -57,21 +57,22 @@ export default function HomePage() {
     fetchData();
   }, []);
 
-  const sliderSettings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    responsive: [
-      { breakpoint: 1200, settings: { slidesToShow: 4, slidesToScroll: 4 } },
-      { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 3 } },
-      { breakpoint: 768, settings: { slidesToShow: 2, slidesToScroll: 2 } },
-      { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
-    ],
-  };
+const sliderSettings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 5, // desktop default
+  slidesToScroll: 5,
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
+  responsive: [
+    { breakpoint: 1200, settings: { slidesToShow: 4, slidesToScroll: 4 } },
+    { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 3 } },
+    { breakpoint: 768, settings: { slidesToShow: 2, slidesToScroll: 2 } }, // tablet
+    { breakpoint: 480, settings: { slidesToShow: 2, slidesToScroll: 2 } }, // mobile: 2 products
+  ],
+};
+
 
   function NextArrow(props) {
     const { onClick } = props;
