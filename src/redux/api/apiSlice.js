7a1b87609af858032fbe908d083e461a83543803
Pranay Jindam <pranayjindam0708@gmail.com>
2025-09-11 17:57:39 +0530

    11-09 5:57

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -4,8 +4,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://mustaab.onrender.com/api",
-    baseUrl: "http://localhost:2000/api",
+    baseUrl: "https://mustaab.onrender.com/api",
+    // baseUrl: "http://localhost:2000/api",
     prepareHeaders: (headers, { getState }) => {
       const token = getState()?.auth?.token;
       if (token) headers.set("Authorization", `Bearer ${token}`);
