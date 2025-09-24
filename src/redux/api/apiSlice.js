a8ce892f50077bc76ce9ceb9d28e788d27416bb2
Pranay Jindam <pranayjindam0708@gmail.com>
2025-09-24 16:35:15 +0530

    24-09 3:25

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { logout } from "../slices/authSlice";
 
 const baseQuery = fetchBaseQuery({
-   baseUrl: import.meta.env.VITE_API_BASE_URL, 
+   baseUrl:import.meta.env.VITE_API_BASE_URL, 
   prepareHeaders: (headers, { getState }) => {
     const token = getState()?.auth?.token;
     if (token) {
